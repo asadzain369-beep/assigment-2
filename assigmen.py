9a4ff3f0fa9f16d6080334bd561c34eb45b0ab03
asadzain369-beep <asadzain369@gmail.com>
2026-06-06 13:37:59 +0500

    Add files via upload

---
+++ assigmen.py
@@ -0,0 +1,27 @@
+Name = "Muhammad zain asad"   #string
+Age =  20                      #integer
+City = "Islamabad"             #string
+isStudent = True                     #boolean
+
+#printing the variables
+
+print(Name)
+print(Age)
+print(City)
+print(isStudent)
+
+#checking the data types of the variables
+
+
+name = "Ali"
+
+age = 18
+
+isStudent = True
+
+
+#printing the data types of the variables
+
+print (type(name))
+print (type(age))
+print (type(isStudent))
